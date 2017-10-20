b1f0c5db2b8e92ca6facc6ba53094ace83e3f10d
Sabari C G <c.gsabari1990@gmail.com>
2017-10-20 19:23:35 +0530

    adding Todo demo

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+import TodoApp from './ToDoForm';
+import './index.css';
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
