c8d461d64e0103e15fda081154f72f87c5d39dba
Sabari C G <c.gsabari1990@gmail.com>
2017-10-20 19:17:56 +0530

    adding SPA demo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Simple SPA</h3>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/stuff">Stuff</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className="content">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
