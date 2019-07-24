e1dda617f9b012f670fa58b3037eadabc26da807
Sabari C G <c.gsabari1990@gmail.com>
2019-07-24 23:58:33 +0530

    added SOW Run Registration app

---
+++ SOWRunRegistrationApp/SOWRunRegistrationApp/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SOWRunRegistrationApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Models.Employee employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                TempData["Employee"] = employee;
+                return RedirectToAction("Acknowledgement");
+            }
+        }
+
+        public ActionResult Acknowledgement()
+        {
+            return View((Models.Employee)TempData["Employee"]);
+        }
+    }
+}
---
+++ SOWRunRegistrationApp/SOWRunRegistrationApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SOWRunRegistrationApp.Models
+{
+    public class Employee
+    {
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter only valid Employee Number")]
+        [Required(ErrorMessage = "Employee Number required")]
+        public int EmpNo { get; set; }
+
+        [DataType(DataType.Text, ErrorMessage = "Employee Name is not valid")]
+        [Required(ErrorMessage = "Employee Name required")]
+        public string EmpName { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email ID is not valid")]
+        [Required(ErrorMessage = "Employee Email ID required")]
+        public string EmpEmailID { get; set; }
+    }
+}
