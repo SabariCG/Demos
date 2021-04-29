1f2f645b82461c5f136fb03014e4f6156ecabfae
Sabari C G <c.gsabari1990@gmail.com>
2021-04-29 23:20:13 +0530

    adding project files

---
+++ DemoConsoleDotNetCore/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Serilog;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using DotNetCoreConsoleLib;
+
+namespace DemoConsoleDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.RollingFile(@"C:\Share\Test\ConsoleLogging.log")
+                .CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+
+            try
+            {
+                logger.LogInformation("Starting...");
+
+                serviceProvider.GetService<FileRetension>().Process(@"C:\Share\Test", 7);
+
+                logger.LogInformation("Done.");
+            }
+            catch(Exception ex)
+            {
+                logger.LogInformation("Error occurred...");
+                logger.LogError(ex, ex.Message);
+                logger.LogInformation(ex.Message);
+            }
+        }
+
+        static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddLogging(configure => configure.AddSerilog())
+                .AddTransient<FileRetension>();
+        }
+    }
+}
---
+++ DotNetCoreConsoleLib/FileRetension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+
+namespace DotNetCoreConsoleLib
+{
+    public class FileRetension
+    {
+        private readonly ILogger<FileRetension> _logger;
+
+        public FileRetension(ILogger<FileRetension> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Process(string folderPath, int retensionPeriodInDays)
+        {
+            if(!Directory.Exists(folderPath))
+            {
+                _logger.LogInformation($"{folderPath} is not found.");
+            }
+            else
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+
+                foreach(DirectoryInfo dirInfo in directoryInfo.GetDirectories())
+                {
+                    _logger.LogInformation($"Checking {dirInfo.Name} folder...");
+
+                    DeleteFiles(dirInfo, retensionPeriodInDays);
+                }
+
+                _logger.LogInformation($"Checking {directoryInfo.Name} folder...");
+
+                DeleteFiles(directoryInfo, retensionPeriodInDays);
+            }
+        }
+
+        private void DeleteFiles(DirectoryInfo dirInfo, int retensionPeriodInDays)
+        {
+            var filteredFiles = Directory.GetFiles(dirInfo.FullName).Where(f => new FileInfo(f).LastWriteTime <= DateTime.Now.AddDays(-(retensionPeriodInDays)));
+
+            if(filteredFiles.Count() > 0)
+            {
+                _logger.LogInformation($"Deleting {filteredFiles.Count()} files from {dirInfo.FullName} path.");
+
+                foreach(var file in filteredFiles)
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+    }
+}
